1e3422bd9e0e47404f1685b81e055510dc6c5279
b00009 <mwcothran@fullsail.edu>
2013-08-29 10:14:51 -0500

    email and array val working

--- Assignment4.js
+++ Assignment4.js
@@ -7,8 +7,50 @@ var checkURL = function (string) {
 	
 	if ((string.substring(0,7) == "http://") || (string.substring(0,8) == "https://")) {
 		return  (URL + " is a valid URL.");
-	} else {
+	}
+	else {
 		return  (URL + " is not a valid URL.");
+	}
+}
+
+
+
+
+// Does a string follow an aaa@bbb.ccc pattern like an email address?
+var checkEmail = function (string) {
+var at = [];
+	for (var i = 0; i < (string.length); i++) {
+		if (string.charAt(i) == "@") {
+			at.push(i);
+		}
+	}
+	if (at.length == 1 && at[0] != 0) {
+		if (string.charAt(string.length - 3) == "." || string.charAt(string.length - 4) == ".") {
+			return (email + " is a valid email.");
+	}
+		else {
+			return (email + " is not a valid email.");	
+		}
+	}
+	else {
+		return (email + " is not a valid email.");	
+	}
+}
+
+
+// Find the smallest value in an array that is greater than a given number
+var findVal = function (array, num) {
+	// sort
+	var compare = function (a, b) {
+		return a - b;
+	}
+	array.sort(compare);
+
+	// Loop 
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] > num) {
+			return array[i];
+		}
 	}
 }
 
@@ -37,13 +79,11 @@ var checkURL = function (string) {
 
 
 
-
-
-
-
-
-
-
-
 var URL = "Http://www.fullsail.com/";
 console.log(checkURL(URL));
+
+var email = "aname@fullsail.edu";
+console.log(checkEmail(email));
+
+var array = [12, 15, 7, 34, 22, 78];
+console.log(findVal(array, 10));
